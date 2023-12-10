280a2f92dfd2e6838fc730496bce70efd0cf4dce
Ynguimaraes <yngrid.guimaraes@mail.uft.edu.br>
2023-12-10 20:28:25 -0300

    mensagens para o usuário

--- math-app.py
+++ math-app.py
@@ -11,3 +11,10 @@ def divisao(a, b):
     if b == 0:
         return "Não é possível dividir por zero."
     return a / b
+
+print("Calculadora Simples")
+print("Escolha a operação:")
+print("1 - Adição")
+print("2 - Subtração")
+print("3 - Multiplicação")
+print("4 - Divisão")
