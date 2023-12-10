db4cb2e3dae45dbf4a596591b354c1cc67f7de6c
Ynguimaraes <yngrid.guimaraes@mail.uft.edu.br>
2023-12-10 20:24:49 -0300

    alterações

--- math-app.py
+++ math-app.py
@@ -11,26 +11,3 @@ def divisao(a, b):
     if b == 0:
         return "Não é possível dividir por zero."
     return a / b
-
-print("Calculadora Simples")
-print("Escolha a operação:")
-print("1 - Adição")
-print("2 - Subtração")
-print("3 - Multiplicação")
-print("4 - Divisão")
-
-opcao = input("Digite o número da operação desejada: ")
-
-num1 = float(input("Digite o primeiro número: "))
-num2 = float(input("Digite o segundo número: "))
-
-if opcao == '1':
-    print("Resultado da adição:", adicao(num1, num2))
-elif opcao == '2':
-    print("Resultado da subtração:", subtracao(num1, num2))
-elif opcao == '3':
-    print("Resultado da multiplicação:", multiplicacao(num1, num2))
-elif opcao == '4':
-    print("Resultado da divisão:", divisao(num1, num2))
-else:
-    print("Opção inválida.")
